70262e0db3bd4f058f2e9f906584304d5bfacbd1
Scott Yu <scott.tj.yu@gmail.com>
2021-01-14 16:06:39 -0800

    feat(animationmanager): update to handle looping animation

--- src/Core/AnimationManager.js
+++ src/Core/AnimationManager.js
@@ -3,24 +3,40 @@ import * as Constants from '../Constants';
 export class AnimationManager {
     index = 0;
     currentFrame;
+    lastLoopFrameStartTime;
 
     constructor() {}
 
-    animate(entity, currentAnimation, startTime, duration) {
+    animate(entity, currentAnimation, startTime, duration, loop = false) {
+        console.log('currentAnimation', currentAnimation);
         let animationSeq = Constants.ANIMATION_TYPES[currentAnimation];
         let frames = animationSeq.length;
         let durationPerFrame = duration / frames;
         let timeNow = performance.now();
-
         this.currentFrame = this.index + 1;
 
-        if (timeNow > startTime + durationPerFrame * this.currentFrame) {
-            this.index += 1;
-        }
-        if (this.currentFrame === frames) {
-            entity.isAnimating = false;
-            entity.currentAnimation = null;
-            this.index = 0;
+        if (loop) {
+            this.lastLoopFrameStartTime
+                ? this.lastLoopFrameStartTime
+                : (this.lastLoopFrameStartTime = startTime);
+            if (timeNow > this.lastLoopFrameStartTime + durationPerFrame * this.currentFrame) {
+                this.lastLoopFrameStartTime = timeNow;
+                this.index += 1;
+            }
+            if (this.currentFrame === frames) {
+                this.lastLoopFrameStartTime = timeNow;
+                this.index = 0;
+                entity.isAnimating = true;
+            }
+        } else {
+            if (timeNow > startTime + durationPerFrame * this.currentFrame) {
+                this.index += 1;
+            }
+            if (this.currentFrame === frames) {
+                entity.isAnimating = false;
+                entity.currentAnimation = null;
+                this.index = 0;
+            }
         }
 
         entity.assetName = animationSeq[this.index];

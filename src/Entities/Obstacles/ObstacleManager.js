1b6754be0502a74cb1317816726da58ad130da93
Scott Yu <scott.tj.yu@gmail.com>
2021-01-19 11:26:30 -0800

    fix(obstaclemanager): fix cannot read property when previousGameWindow is null

--- src/Entities/Obstacles/ObstacleManager.js
+++ src/Entities/Obstacles/ObstacleManager.js
@@ -1,6 +1,6 @@
 import * as Constants from '../../Constants';
 import { randomInt } from '../../Core/Utils';
-import { Obstacle } from "./Obstacle";
+import { Obstacle } from './Obstacle';
 
 const DISTANCE_BETWEEN_OBSTACLES = 50;
 const STARTING_OBSTACLE_GAP = 100;
@@ -10,8 +10,7 @@ const NEW_OBSTACLE_CHANCE = 8;
 export class ObstacleManager {
     obstacles = [];
 
-    constructor() {
-    }
+    constructor() {}
 
     getObstacles() {
         return this.obstacles;
@@ -24,14 +23,17 @@ export class ObstacleManager {
     }
 
     placeInitialObstacles() {
-        const numberObstacles = Math.ceil((Constants.GAME_WIDTH / STARTING_OBSTACLE_REDUCER) * (Constants.GAME_HEIGHT / STARTING_OBSTACLE_REDUCER));
+        const numberObstacles = Math.ceil(
+            (Constants.GAME_WIDTH / STARTING_OBSTACLE_REDUCER) *
+                (Constants.GAME_HEIGHT / STARTING_OBSTACLE_REDUCER)
+        );
 
         const minX = -Constants.GAME_WIDTH / 2;
         const maxX = Constants.GAME_WIDTH / 2;
         const minY = STARTING_OBSTACLE_GAP;
         const maxY = Constants.GAME_HEIGHT / 2;
 
-        for(let i = 0; i < numberObstacles; i++) {
+        for (let i = 0; i < numberObstacles; i++) {
             this.placeRandomObstacle(minX, maxX, minY, maxY);
         }
 
@@ -42,31 +44,41 @@ export class ObstacleManager {
 
     placeNewObstacle(gameWindow, previousGameWindow) {
         const shouldPlaceObstacle = randomInt(1, NEW_OBSTACLE_CHANCE);
-        if(shouldPlaceObstacle !== NEW_OBSTACLE_CHANCE) {
+        if (shouldPlaceObstacle !== NEW_OBSTACLE_CHANCE) {
             return;
         }
 
-        if(gameWindow.left < previousGameWindow.left) {
-            this.placeObstacleLeft(gameWindow);
-        }
-        else if(gameWindow.left > previousGameWindow.left) {
-            this.placeObstacleRight(gameWindow);
-        }
+        if (previousGameWindow) {
+            if (gameWindow.left < previousGameWindow.left) {
+                this.placeObstacleLeft(gameWindow);
+            } else if (gameWindow.left > previousGameWindow.left) {
+                this.placeObstacleRight(gameWindow);
+            }
 
-        if(gameWindow.top < previousGameWindow.top) {
-            this.placeObstacleTop(gameWindow);
+            if (gameWindow.top < previousGameWindow.top) {
+                this.placeObstacleTop(gameWindow);
+            } else if (gameWindow.top > previousGameWindow.top) {
+                this.placeObstacleBottom(gameWindow);
+            }
         }
-        else if(gameWindow.top > previousGameWindow.top) {
-            this.placeObstacleBottom(gameWindow);
-        }
-    };
+    }
 
     placeObstacleLeft(gameWindow) {
-        this.placeRandomObstacle(gameWindow.left, gameWindow.left, gameWindow.top, gameWindow.bottom);
+        this.placeRandomObstacle(
+            gameWindow.left,
+            gameWindow.left,
+            gameWindow.top,
+            gameWindow.bottom
+        );
     }
 
     placeObstacleRight(gameWindow) {
-        this.placeRandomObstacle(gameWindow.right, gameWindow.right, gameWindow.top, gameWindow.bottom);
+        this.placeRandomObstacle(
+            gameWindow.right,
+            gameWindow.right,
+            gameWindow.top,
+            gameWindow.bottom
+        );
     }
 
     placeObstacleTop(gameWindow) {
@@ -74,7 +86,12 @@ export class ObstacleManager {
     }
 
     placeObstacleBottom(gameWindow) {
-        this.placeRandomObstacle(gameWindow.left, gameWindow.right, gameWindow.bottom, gameWindow.bottom);
+        this.placeRandomObstacle(
+            gameWindow.left,
+            gameWindow.right,
+            gameWindow.bottom,
+            gameWindow.bottom
+        );
     }
 
     placeRandomObstacle(minX, maxX, minY, maxY) {
@@ -90,20 +107,19 @@ export class ObstacleManager {
 
         const foundCollision = this.obstacles.find((obstacle) => {
             return (
-                x > (obstacle.x - DISTANCE_BETWEEN_OBSTACLES) &&
-                x < (obstacle.x + DISTANCE_BETWEEN_OBSTACLES) &&
-                y > (obstacle.y - DISTANCE_BETWEEN_OBSTACLES) &&
-                y < (obstacle.y + DISTANCE_BETWEEN_OBSTACLES)
+                x > obstacle.x - DISTANCE_BETWEEN_OBSTACLES &&
+                x < obstacle.x + DISTANCE_BETWEEN_OBSTACLES &&
+                y > obstacle.y - DISTANCE_BETWEEN_OBSTACLES &&
+                y < obstacle.y + DISTANCE_BETWEEN_OBSTACLES
             );
         });
 
-        if(foundCollision) {
+        if (foundCollision) {
             return this.calculateOpenPosition(minX, maxX, minY, maxY);
-        }
-        else {
+        } else {
             return {
                 x: x,
-                y: y
+                y: y,
             };
         }
     }
